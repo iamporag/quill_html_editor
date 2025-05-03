0224a4f04e7d62db4ce316a77a7b593253c18fc6
Sakib Hasan <sakibporag24@gmail.com>
2025-05-03 13:10:15 +0600

    Fix platform declaration for Flutter 3.29

---
+++ lib/plugin_registrar.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+/// This class is used by Flutter to register Dart-side platform plugins.
+class QuillHtmlEditor {
+  static void registerWith(Registrar registrar) {
+    // No-op (or you can register a web implementation if needed)
+  }
+}
